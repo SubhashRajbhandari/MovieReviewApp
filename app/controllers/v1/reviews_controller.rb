9c7301a99b5228b023bd9e1f87af6b4790fabb63
SubhashRajbhandari <subhash.link123@gmail.com>
2024-04-04 14:05:25 +0545

    post request api done without auto movie add feature

--- app/controllers/v1/reviews_controller.rb
+++ app/controllers/v1/reviews_controller.rb
@@ -1,9 +1,53 @@
 class V1::ReviewsController < ApplicationController
-  def index
-    movie = Movie.find(params[:movie_id])
-    reviews = movie.reviews.map { |review| { user_id: review.user_id, thereview: review.description } }
-    render json: { mid: movie.id, reviews: reviews }
-  rescue ActiveRecord::RecordNotFound
-    render json: { error: 'Movie not found' }, status: :not_found
+  # skip_before_action :authenticate_user!, only: [:create]
+  skip_before_action :verify_authenticity_token, only: [:create]
+    def index
+      #  movie = Movie.find(params[:movie_id])
+
+      movie = Movie.find_by(mid: params[:movie_id])
+      if movie.nil?
+
+        @movie = fetch_movie(params[:movie_id])
+          if @movie['id'].nil?
+            render json: { error: 'movie not found in tmdb' }, status: :not_found
+              else
+                  @movie = Movie.find_by(mid: @movie['id']) || Movie.create(mid: @movie['id'], title: @movie['title'], original_title: @movie['original_title'], overview: @movie['overview'], poster: @movie['poster_path'], popularity: @movie['popularity'])
+
+                  reviews = @movie.reviews.map { |review| { user_id: review.user_id, thereview: review.description } }
+                  render json: {TotalReview: reviews.count, movie_id: @movie.mid, reviews: reviews }
+              end
+            else
+          reviews = movie.reviews.map { |review| { user_id: review.user_id, thereview: review.description } }
+          render json: {TotalReview: reviews.count, movie_id: movie.mid, reviews: reviews }
+          end
   end
+
+def create
+  movie = Movie.find_by(mid: params[:movie_id])
+
+  if movie.nil?
+    render json: { error: 'Movie not found' }, status: :not_found
+  else
+    review = movie.reviews.build(review_params)
+    review.user_id = 1
+    if review.save
+      render json: { message: 'Review created successfully', review_id: review.id }, status: :created
+    else
+      render json: { error: 'Failed to create review', errors: review.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+end
+
+private
+
+def review_params
+  params.require(:review).permit(:description)
+end
+
+private
+
+def fetch_movie(movie_id)
+  Tmdb::DetailService.execute(id: movie_id)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,12 @@ Rails.application.routes.draw do
   get 'home/index'
 
   namespace :v1 do
-    get 'reviews/index'
-    resources :movies, param: :mid, only: [] do
-      resources :reviews, only: [:index]
+    resources :movies  do
+      resources :reviews
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # post '/movies/:movie_id/reviews', to: 'movies#create_review'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
